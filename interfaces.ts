fe5bd40db7fd426d8180386e8a14cce3d2c5fe41
DKLokeswaran <lokeswarankandasamy12@gmail.com>
2024-10-12 19:18:34 +0530

    Added Heading map interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,9 +1,16 @@
 import { HeadingCache } from "obsidian";
+
 export interface TOCProps {
   headings: HeadingCache[]|undefined|null;
   filePath: string
 }
+
 export interface LinkProps {
   headings: HeadingCache[];
   filePath: string
+}
+
+export interface HeadingMap {
+  headingCache : HeadingCache;
+  element : JSX.Element
 }
